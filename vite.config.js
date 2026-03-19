d9e3a378e99ad157192b03d0187b4c5bf56bbb57
ytung01 <yufan.tung@yahooinc.com>
2026-03-19 11:22:19 +0800

    Fix

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,5 @@ export default defineConfig({
   plugins: [react()],
   // 這裡的名稱請改成你的 GitHub 倉庫 (Repository) 名稱
   // 例如你的 Repo 叫 my-itinerary，這裡就填 '/my-itinerary/'
-  base: '/f1-travel-schedule', 
+  base: '/f1-travel-schedule/',
 })
